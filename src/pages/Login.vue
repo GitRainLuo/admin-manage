891b1646f3ad0283bbc79cea9dcd66283d6b4370
hmj <1004033345@qq.com>
2018-10-10 17:21:08 +0800

    1.webpack.base.config.js:安装引入scss语法 2.config index.js:修改为自动打开浏览器 3.登录页面(界面) 4.router index.js：登录页面路由 默认打开就是登录页面

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,19 +1,77 @@
 <template>
-    <el-form>
-
-    </el-form>
+  <el-form :model="userForm" ref="userForm" :rules="rules1" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">登录系统</h3>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="userForm.account" autocomplete="off" placeholder="请输入账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="userForm.password" autocomplete="off" placeholder="请输入密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width: 100%">
+      <el-button type="primary" style="width: 100%" :loading="logining" @click.native.prevent="handleSubmit">登录</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script>
-    export default{
-        data () {
-            return {msg: '这个是Home模板页'}
+  import ElForm from "../../node_modules/element-ui/packages/form/src/form";
+  import ElFormItem from "../../node_modules/element-ui/packages/form/src/form-item";
+  import ElInput from "../../node_modules/element-ui/packages/input/src/input";
+  import ElCheckbox from "../../node_modules/element-ui/packages/checkbox/src/checkbox";
+  import ElButton from "../../node_modules/element-ui/packages/button/src/button";
+  export default{
+    components: {
+      ElButton,
+      ElCheckbox,
+      ElInput,
+      ElFormItem,
+      ElForm
+    },
+    data () {
+      return {
+        userForm:{
+          account:'admin',
+          password:'123456'
+        },
+        checked:true,
+        logining:false,
+        rules1:{
+          //输入时候的验证
+          account:[
+            {required:true,message:'请输入账号',trigger:'blur'}
+          ],
+          password:[
+            {required:true,message:'请输入密码',trigger:'blur'}
+          ]
         }
+      }
+    },
+    methods:{
+      handleSubmit:function () {
+        console.log("1")
+      }
     }
+  }
 </script>
 
-<style>
-    body {
-        background-color: #ff0000;
+<style lang="scss" scoped>
+  .login-container{
+    width: 300px;
+    height: auto;
+    border-radius: 3px;
+    -webkit-boder-radius:3px;
+    -moz-boder-radius:3px;
+    border: 1px solid #eaeaea;
+    margin: 0 auto;
+    padding: 20px 30px 10px 30px;
+    .title{
+      text-align: center;
+      color: #505458;
     }
+    .remember{
+      float: left;
+      margin: 0px 0px 20px 0px;
+    }
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/Login'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      //进入时默认为登录页面
+      redirect:'/login'
+    },
+    {
+      path:'/login',
+      name:'Login',
+      component:Login
     }
   ]
 })
