e35e4545964f22206cf33dc5dbdad87f5a483a1a
hmj <1004033345@qq.com>
2018-10-10 14:04:13 +0800

    初始化

---
+++ src/pages/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-form>
+
+    </el-form>
+</template>
+
+<script>
+    export default{
+        data () {
+            return {msg: '这个是Home模板页'}
+        }
+    }
+</script>
+
+<style>
+    body {
+        background-color: #ff0000;
+    }
+</style>
